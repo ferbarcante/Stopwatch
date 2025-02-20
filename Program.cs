605423a2a82621fa98229aad4e4e8e7cef451a1e
ferbarcante <barcantef@gmail.com>
2025-02-20 16:24:08 -0300

    fazendo menu

--- Program.cs
+++ Program.cs
@@ -7,15 +7,36 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            StartStopwatch();
+            Menu();
         }
 
-        static void StartStopwatch()
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if(type == 'm')
+              multiplier = 60;
+
+            if(time == 0)
+              System.Environment.Exit(0);
+
+            StartStopwatch(time * multiplier);
+        }
+
+        static void StartStopwatch(int time)
         {
             Console.Clear();
             Console.WriteLine("Stopwatch started!");
-
-            int time = 10;
             
             while(time > 0)
             {
@@ -25,6 +46,11 @@ namespace Stopwatch
                 Console.WriteLine($"{time} seconds remaining...");
                 Thread.Sleep(1000);
             }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch stopped!");
+            Thread.Sleep(2500);
+            Menu();
         }   
     }
 }
