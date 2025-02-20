30587a73696abb517ba2be346a414243012005c1
ferbarcante <barcantef@gmail.com>
2025-02-20 16:00:14 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            StartStopwatch();
+        }
+
+        static void StartStopwatch()
+        {
+            Console.Clear();
+            Console.WriteLine("Stopwatch started!");
+
+            int time = 10;
+            
+            while(time > 0)
+            {
+                Console.Clear();
+                time--;
+
+                Console.WriteLine($"{time} seconds remaining...");
+                Thread.Sleep(1000);
+            }
+        }   
+    }
+}
